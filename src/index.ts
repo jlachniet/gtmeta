5eeca0093bd9590386617e975309356a8bdd0e4b
Julian Lachniet <julian@lachniet.com>
2024-09-01 23:30:09 -0400

    feat: Add a usage message when the CLI is ran

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 #!/usr/bin/env node
 
+export const VERSION = '0.1.0';
+export const USAGE_MESSAGE = `gtmeta ${VERSION}
+
+Usage:
+  $ gtmeta <configFile> <outputFolder>
+
+Arguments:
+  configFile      The path to the config file
+  outputFolder    The path to the output folder`;
+
 /**
  * The main function.
  */
 export function main() {
-	console.log('Hello, World!');
+	console.info(USAGE_MESSAGE);
 }
 
 main();
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,11 +1,17 @@
 import { describe, expect, it, vi } from 'vitest';
-import { main } from '../index.js';
+import { main, USAGE_MESSAGE, VERSION } from '../index.js';
+
+describe('VERSION', () => {
+	it('should match the package.json version', () => {
+		expect(process.env['npm_package_version']).toBe(VERSION);
+	});
+});
 
 describe('main()', () => {
-	const consoleLogSpy = vi.spyOn(console, 'log');
+	const consoleLogSpy = vi.spyOn(console, 'info');
 
-	it('should log "Hello, World!"', () => {
+	it('should log the usage message', () => {
 		main();
-		expect(consoleLogSpy).toHaveBeenCalledWith('Hello, World!');
+		expect(consoleLogSpy).toHaveBeenCalledWith(USAGE_MESSAGE);
 	});
 });
