91aad9249bebad3371fd517c212d9a7acc171447
Julian Lachniet <julian@lachniet.com>
2024-09-02 21:30:03 -0400

    test: Add tests for Logger

--- src/tests/logging.test.ts
+++ src/tests/logging.test.ts
@@ -1,8 +1,31 @@
-import { describe, expect, it } from 'vitest';
-import { VERSION } from '../logging.js';
+import chalk from 'chalk';
+import { describe, expect, it, vi } from 'vitest';
+import { Logger, USAGE_MESSAGE, VERSION } from '../logging.js';
 
 describe('VERSION', () => {
 	it('should match the package.json version', () => {
 		expect(process.env['npm_package_version']).toBe(VERSION);
+	});
+});
+
+describe('Logger', () => {
+	describe('usage', () => {
+		it('should log the usage message', () => {
+			const consoleInfoSpy = vi.spyOn(console, 'info');
+
+			Logger.usage();
+			expect(consoleInfoSpy).toHaveBeenCalledWith(USAGE_MESSAGE);
+		});
+	});
+
+	describe('error', () => {
+		it('should log an error message', () => {
+			const consoleErrorSpy = vi.spyOn(console, 'error');
+
+			Logger.error('message');
+			expect(consoleErrorSpy).toHaveBeenCalledWith(
+				`${chalk.red('error')} message`,
+			);
+		});
 	});
 });
